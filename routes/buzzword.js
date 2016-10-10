00fa05fab83c525d8e96dd3a77710073c84a6d62
Bryan Choe <bryan.choe88@gmail.com>
2016-10-09 15:04:16 -1000

    enabled buzzword route

--- routes/buzzword.js
+++ routes/buzzword.js
@@ -1,21 +1,75 @@
-/*const express = require('express');
+const express = require('express');
 const router = express.Router();
-let buzzWords = [];
 
+let wordArr = [];
+let newScore = 0;
+
+function createBuzzWord(buzzWord, points){
+  let buzzWords = {
+    buzzWord,
+    points,
+    heard: false
+  };
+  wordArr.push(buzzWords);
+  return {success: true};
+}
+
+function getBuzzWord(element){
+  let selectedIndex = -1;
+  wordArr.forEach((buzz) => {
+    if(buzz.buzzWords === element.buzzWords){
+      selectedIndex = wordArr.indexOf(buzz);
+    }
+  });
+  return selectedIndex;
+}
+
+//to access -----> localhost:1487/buzzwords/jon
+router.route('/jon').get((req,res)=> {
+  res.send('hello jon');
+});
+//to access ----> localhost:1487/buzzwords
 router.route('/')
 
+  .get((req,res) => {
+    let words = wordArr.map((element) => {
+      return element.buzzWord;
+    });
+    res.json(words);
+  })
+
   .post((req,res) => {
-    console.log('req.body:', req.body);
-    buzzWords.push(req.body);
-    res.send('{"success": true}');
+    let duplicateFound = wordArr.find((word) => {
+      return word.buzzWord === req.body.buzzWord;
+    });
+    if(!duplicateFound){
+      createBuzzWord(req.body.buzzWord, req.body.points);
+    }
+    return res.json({
+      success: !duplicateFound
+    });
+  })
+
+  .put((req,res) => {
+    let selectedBuzzword = getBuzzWord(req.body);
+    if(selectedBuzzword > -1){
+      let score = parseFloat(wordArr[selectedBuzzword].points);
+      score += parseFloat(req.body.points);
+      wordArr[selectedBuzzword].points = score;
+      res.send(`{"success": true, newScore: ${score} }`);
+    } else {
+      res.send('{"success": false}');
+    }
+  })
+
+  .delete((req,res) => {
+    let selectedBuzzword = getBuzzWord(req.body);
+    if(selectedBuzzword > -1){
+      wordArr.splice(selectedBuzzword, 1);
+      res.send(`{"success": true}`);
+    } else {
+      res.send('{"success": false}');
+    }
   });
 
-
-
-
-router.get('/:id', (req, res) => {
-  console.log("req.params.id: ", req.params.id);
-  res.send(req.params.id);
-});
-
-module.exports = {router: router, buzzwords: buzzWords};*/
+module.exports = router;
--- server.js
+++ server.js
@@ -2,74 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = 1487;
 const bodyParser = require('body-parser');
-let wordArr = [];
-let newScore = 0;
 
-function createBuzzWord(buzzWord, points){
-  let buzzWords = {
-    buzzWord,
-    points,
-    heard: false
-  };
-  wordArr.push(buzzWords);
-  return {success: true};
-}
-
-function getBuzzWord(element){
-  let selectedIndex = -1;
-  wordArr.forEach((buzz) => {
-    if(buzz.buzzWords === element.buzzWords){
-      selectedIndex = wordArr.indexOf(buzz);
-    }
-  });
-  return selectedIndex;
-}
+const buzzword = require('./routes/buzzword.js');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
-app.route('/buzzwords')
-
-  .get((req,res) => {
-    let words = wordArr.map((element) => {
-      return element.buzzWord;
-    });
-    res.json(words);
-  })
-
-  .post((req,res) => {
-    let duplicateFound = wordArr.find((word) => {
-      return word.buzzWord === req.body.buzzWord;
-    });
-    if(!duplicateFound){
-      createBuzzWord(req.body.buzzWord, req.body.points);
-    }
-    return res.json({
-      success: !duplicateFound
-    });
-  })
-
-  .put((req,res) => {
-    let selectedBuzzword = getBuzzWord(req.body);
-    if(selectedBuzzword > -1){
-      let score = parseFloat(wordArr[selectedBuzzword].points);
-      score += parseFloat(req.body.points);
-      wordArr[selectedBuzzword].points = score;
-      res.send(`{"success": true, newScore: ${score} }`);
-    } else {
-      res.send('{"success": false}');
-    }
-  })
-
-  .delete((req,res) => {
-    let selectedBuzzword = getBuzzWord(req.body);
-    if(selectedBuzzword > -1){
-      wordArr.splice(selectedBuzzword, 1);
-      res.send(`{"success": true}`);
-    } else {
-      res.send('{"success": false}');
-    }
-  });
+app.use('/buzzwords',buzzword);
 
 const server = app.listen(PORT, () => {
   console.log(`Server started on ${PORT}`);
